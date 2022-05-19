7004d46d4b1856ba15be8fb69c140e9503991b7d
Aljhsrdnl <blackgreen0721@gmail.com>
2022-05-19 21:07:17 +0800

    started frontend

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import './HomeScreen.css';
+
+const HomeScreen = () => {
+    return (
+        <div className="homescreen">
+            Test
+        </div>
+    )
+}
+
+export default HomeScreen
